c5cc63e686fa74e7927b458d788ef30e3d0b1e96
gxqeg51640 <140863220+gxqeg51640@users.noreply.github.com>
2023-07-29 06:30:14 +0600

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-G_DRIVE_CLIENT_ID = "" 
-G_DRIVE_CLIENT_SECRET = ""
+G_DRIVE_CLIENT_ID = "1092870962484-0cmn7okeip96jbj6jvioridk5shdqs6a.apps.googleusercontent.com" 
+G_DRIVE_CLIENT_SECRET = "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"
 app_url = 'http://127.0.0.1:5200'
-service_acc_google_group_email = '' # optional
+service_acc_google_group_email = 'ornobbot@googlegroups.com' # optional
